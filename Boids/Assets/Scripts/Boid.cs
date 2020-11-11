555cb47642e55031529d89d981c0fadb4afae2d6
Smos Bois <simondeb1505@gmail.com>
2020-11-11 19:29:46 +0100

    init

---
+++ Boids/Assets/Scripts/Boid.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid
+{
+    public Vector3 velocity;
+    public GameObject boidBody;
+    public Boid(GameObject body)
+    {
+        boidBody = body;
+    }
+    public Vector3 Rule1(List<Boid> allTheBoids)
+    {
+        Vector3 perceivedCentre = Vector3.zero;
+        foreach (Boid boid in allTheBoids)
+        {
+            if(boid != this)
+            {
+                perceivedCentre = perceivedCentre + boid.boidBody.transform.position;
+            }
+        }
+        Debug.Log(allTheBoids.Count);
+        perceivedCentre = perceivedCentre / (allTheBoids.Count - 1);
+
+        return (perceivedCentre - boidBody.transform.position) / 100;
+    }
+
+    public Vector3 Rule2(List<Boid> allTheBoids)
+    {
+        Vector3 c = Vector3.zero;
+        foreach (Boid boid in allTheBoids)
+        {
+            if(boid != this)
+            {
+                if (Vector3.Magnitude(boid.boidBody.transform.position - boidBody.transform.position) < 300)
+                {
+                    //Debug.Log(Vector3.Distance(boid.boidBody.transform.position, boidBody.transform.position));
+                    c = c - (boid.boidBody.transform.position - boidBody.transform.position) / 100;
+                }
+            }
+        }
+            return c;
+    }
+
+    public Vector3 Rule3(List<Boid> allTheBoids)
+    {
+        Vector3 perceivedVelocity = Vector3.zero;
+        foreach (Boid boid in allTheBoids)
+        {
+            if (boid != this)
+            {
+                perceivedVelocity = perceivedVelocity + boid.velocity;
+            }
+
+            perceivedVelocity = perceivedVelocity / (allTheBoids.Count - 1);
+
+        }
+
+        return (perceivedVelocity - velocity) / 8;
+    }
+}
---
+++ Boids/Assets/Scripts/BoidManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour
+{
+    List<Boid> Boids;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boids = new List<Boid>();
+
+        //Spawn Boids
+        for(int i = 0; i < 20; i++)
+        {
+            GameObject boidBody = Resources.Load("BoidPrefab") as GameObject;
+
+            GameObject bodytemp = Instantiate(boidBody, new Vector3(Random.Range(-10f, 10f), Random.Range(1f, 20f), Random.Range(-10f, 10f)),
+                new Quaternion(Random.Range(-1f,1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f,1f)));
+
+            Boid boidtemp = new Boid(bodytemp);
+            Boids.Add(boidtemp);
+            boidtemp.velocity = bodytemp.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v1, v2, v3;
+        foreach (Boid boid in Boids)
+        {
+            //check rule 1
+            v1 = boid.Rule1(Boids);
+
+            //check rule 2
+            v2 = boid.Rule2(Boids);
+
+            //check rule 3
+            v3 = boid.Rule3(Boids);
+
+            //change position
+            boid.velocity = boid.velocity + v1 + v2 + v3;
+            boid.boidBody.transform.position = boid.boidBody.transform.position + boid.velocity;
+        }
+    }
+}
